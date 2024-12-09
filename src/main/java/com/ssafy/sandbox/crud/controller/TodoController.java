83058bb8d620dcb88d3b6396028a556027dd6c84
aseongjun99 <aseongjun99@gmail.com>
2024-12-10 00:28:01 +0900

    feat: TodoList 조회를 위한 getTodos 메서드 작성

--- src/main/java/com/ssafy/sandbox/crud/controller/TodoController.java
+++ src/main/java/com/ssafy/sandbox/crud/controller/TodoController.java
@@ -1,4 +1,20 @@
 package com.ssafy.sandbox.crud.controller;
 
+import com.ssafy.sandbox.crud.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todos")
+@CrossOrigin(value = "https://ssafysandbox.vercel.app")
 public class TodoController {
+
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<?> getTodos() {
+        return ResponseEntity.ok().body(todoService.getTodos());
+    }
 }
--- src/main/java/com/ssafy/sandbox/crud/service/TodoService.java
+++ src/main/java/com/ssafy/sandbox/crud/service/TodoService.java
@@ -1,4 +1,12 @@
 package com.ssafy.sandbox.crud.service;
 
+import com.ssafy.sandbox.crud.dto.TodoResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
+
+    public TodoResponse getTodos() {
+        return new TodoResponse();
+    }
 }
