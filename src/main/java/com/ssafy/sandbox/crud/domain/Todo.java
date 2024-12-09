9bd2f620354055911bd6aab5991d5002c0117800
aseongjun99 <aseongjun99@gmail.com>
2024-12-09 22:10:53 +0900

    feat: Todo 도메인 구성

--- src/main/java/com/ssafy/sandbox/crud/domain/Todo.java
+++ src/main/java/com/ssafy/sandbox/crud/domain/Todo.java
@@ -1,4 +1,12 @@
 package com.ssafy.sandbox.crud.domain;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Getter
 public class Todo {
+
+    private Integer id;
+    private String content;
+    private Boolean completed;
 }
