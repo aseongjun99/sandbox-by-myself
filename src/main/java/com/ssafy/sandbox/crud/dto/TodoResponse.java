42ecc07bf27d87128c847b0809512b2d0c47ce06
aseongjun99 <aseongjun99@gmail.com>
2024-12-10 00:28:37 +0900

    feat: TodoList를 조회하는 API 응답 객체 작성

--- src/main/java/com/ssafy/sandbox/crud/dto/TodoResponse.java
+++ src/main/java/com/ssafy/sandbox/crud/dto/TodoResponse.java
@@ -1,4 +1,15 @@
 package com.ssafy.sandbox.crud.dto;
 
+import com.ssafy.sandbox.crud.domain.Todo;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class TodoResponse {
+
+    private List<Todo> todos = new ArrayList<>();
 }
