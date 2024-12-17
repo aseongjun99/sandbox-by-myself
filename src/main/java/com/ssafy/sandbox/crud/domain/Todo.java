5638c4bb3509a12a8955f606a4bfcc18778fe866
aseongjun99 <aseongjun99@gmail.com>
2024-12-17 16:57:49 +0900

    refactor: Todo 도메인 엔티티로 설정

--- src/main/java/com/ssafy/sandbox/crud/domain/Todo.java
+++ src/main/java/com/ssafy/sandbox/crud/domain/Todo.java
@@ -1,12 +1,19 @@
 package com.ssafy.sandbox.crud.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column
     private String content;
+
+    @Column
     private Boolean completed;
 }
