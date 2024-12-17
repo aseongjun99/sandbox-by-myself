2dda4f508fde635a23dab34426132407aa4d5a71
aseongjun99 <aseongjun99@gmail.com>
2024-12-17 18:04:26 +0900

    feat: 완료 여부를 변경하기 위한 dto 추가

---
+++ src/main/java/com/ssafy/sandbox/crud/dto/update/UpdateTodoRequest.java
@@ -0,0 +1,4 @@
+package com.ssafy.sandbox.crud.dto.update;
+
+public class UpdateTodoRequest {
+}
---
+++ src/main/java/com/ssafy/sandbox/crud/dto/update/UpdateTodoResponse.java
@@ -0,0 +1,11 @@
+package com.ssafy.sandbox.crud.dto.update;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateTodoResponse {
+
+    private int code;
+}
