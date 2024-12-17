77a0e2cca2aa528c66e3c25dde892083c33c2f86
aseongjun99 <aseongjun99@gmail.com>
2024-12-17 17:12:00 +0900

    refactor: 기존 todo를 읽어오는 부분을 jpa를 사용하도록 설정

--- src/main/java/com/ssafy/sandbox/crud/repository/TodoRepository.java
+++ src/main/java/com/ssafy/sandbox/crud/repository/TodoRepository.java
@@ -1,4 +1,11 @@
 package com.ssafy.sandbox.crud.repository;
 
-public interface TodoRepository {
+import com.ssafy.sandbox.crud.domain.Todo;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
 }
--- src/main/java/com/ssafy/sandbox/crud/service/TodoService.java
+++ src/main/java/com/ssafy/sandbox/crud/service/TodoService.java
@@ -1,12 +1,19 @@
 package com.ssafy.sandbox.crud.service;
 
 import com.ssafy.sandbox.crud.dto.read.TodoResponse;
+import com.ssafy.sandbox.crud.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
+    @Autowired
+    TodoRepository todoRepository;
+
     public TodoResponse getTodos() {
-        return new TodoResponse();
+        TodoResponse todoResponse = new TodoResponse();
+        todoResponse.setTodos(todoRepository.findAll());
+        return todoResponse;
     }
 }
