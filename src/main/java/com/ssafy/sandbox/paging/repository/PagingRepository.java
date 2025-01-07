7921c45e49b06894b15fd4c3e8019fb0e7be850c
aseongjun99 <aseongjun99@gmail.com>
2025-01-07 17:43:41 +0900

    feat: Offset 방식의 페이지네이션을 위한 쿼리 작성

--- src/main/java/com/ssafy/sandbox/paging/repository/PagingRepository.java
+++ src/main/java/com/ssafy/sandbox/paging/repository/PagingRepository.java
@@ -2,6 +2,13 @@ package com.ssafy.sandbox.paging.repository;
 
 import com.ssafy.sandbox.paging.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PagingRepository extends JpaRepository<Article, Integer> {
+
+    @Query(value = "SELECT * FROM article LIMIT :offset , :limit", nativeQuery = true)
+    List<Article> findAllByOffset(@Param("offset") Integer offset, @Param("limit") Integer limit);
 }
